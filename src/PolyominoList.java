80224dc06c272b758b9ab56f5bfee9bb1f02ebe7
Raphaël <erwan.bergot0@gmail.com>
2016-12-11 12:48:02 +0100

    Essai question 2

--- src/PolyominoList.java
+++ src/PolyominoList.java
@@ -22,6 +22,10 @@ public class PolyominoList extends LinkedList<Polyomino>{ //ne gère pas les exce
 		}
 	}
 	
+	PolyominoList(){
+		super();
+	}
+	
 	
 	public void draw(){
 		Image2d img = new Image2d(1000,500); //régler le paramétrage auto
@@ -39,8 +43,8 @@ public class PolyominoList extends LinkedList<Polyomino>{ //ne gère pas les exce
 				img.addPolygon(xc, yc, Color.red);
 			}
 		}
-		Image2dComponent imgC = new Image2dComponent(img); //vérifier si affecter est nécéssaire
-		Image2dViewer imgV = new Image2dViewer(img);
+		new Image2dComponent(img);
+		new Image2dViewer(img);
 	}
 	
 	@Override
@@ -54,13 +58,64 @@ public class PolyominoList extends LinkedList<Polyomino>{ //ne gère pas les exce
 		return s;		
 	}
 	
-	public static void main(String[] args){
+	public static PolyominoList fixedPolyomino(int area, LinkedList<int[]> c_vois){
+		PolyominoList resultat = new PolyominoList();
+		if (area == 1){
+			Polyomino e = new Polyomino("(0,0)");
+			resultat.addFirst(e);
+			return resultat;
+		}
+		
+		for(int i = 0; i < c_vois.size(); i++){
+			int[] Pcase = c_vois.get(i);
+			LinkedList<int[]> c_vois_aux = new LinkedList<int[]>(c_vois);
+			int x = Pcase[0];
+			int y = Pcase[1];
+			int[] voisin1 = {x+1,y};
+			int[] voisin2 = {x,y+1};
+			int[] voisin3 = {x-1,y};
+			int[] voisin4 = {x,y-1};
+			c_vois_aux.add(voisin1);
+			c_vois_aux.add(voisin2);
+			c_vois_aux.add(voisin3);
+			c_vois_aux.add(voisin4);
+			
+			
+			for(int j = 0; j < c_vois_aux.size(); j++){
+				int[] aux = c_vois_aux.get(j);
+				if(aux == Pcase){
+					c_vois_aux.remove(i);
+				}
+			}
+			
+			PolyominoList resultat_aux = fixedPolyomino(area-1, c_vois_aux);
+			for(int k = 0; k < resultat_aux.size(); k++){
+				Polyomino p = resultat_aux.get(k);
+				p.liste.add(Pcase);
+			}
+			resultat.addAll(resultat_aux);
+		}
+		return resultat;
+	}
+	
+	public static void test1(){
 		PolyominoList test = new PolyominoList("src/polyominoesINF421.txt");
 		for(int i = 0; i < test.size(); i++){
 			Polyomino p = test.get(i);
 			p.translater(5*i, 0);
 		}
 		test.draw();
+	}
+	
+	public static void main(String[] args){
+		//test1();
+		int[] a1 = {0,1};
+		int[] a2 = {1,0};
+		LinkedList<int[]> b = new LinkedList<int[]>();
+		b.add(a1);
+		b.add(a2);
+		PolyominoList test2 = fixedPolyomino(3,b);
+		System.out.print(test2);
 		
 	}
 }
